aedd4fdb43e7f82ccd1abcf0d448e263db0bacf4
RafaelMQs <rafaeelmq08@gmail.com>
2022-01-31 16:52:52 -0300

    Criando uma pagina de Profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Home', url: '', icon: 'home' }
+    { title: 'Profile', url: 'profile', icon: 'person-circle' },
+    { title: 'Categorias', url: 'categorias', icon: 'apps' }
   ];
   constructor() { }
 }
