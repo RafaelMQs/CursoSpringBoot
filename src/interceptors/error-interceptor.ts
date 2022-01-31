47dc81645835e2574d6a7fe4f43808ae15d6cd7d
RafaelMQs <rafaeelmq08@gmail.com>
2022-01-31 15:39:41 -0300

    Criando Interceptor para tratamento de erros

---
+++ src/interceptors/error-interceptor.ts
@@ -0,0 +1,31 @@
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { catchError } from 'rxjs/operators';
+import { Observable, throwError } from "rxjs";
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log("Passou pelo interceptor!")
+    return next.handle(req)
+      .pipe(
+        catchError(error => {
+
+          if (!error.status) {
+            error = JSON.parse(error);
+          }
+
+          console.log("Erro detectado pelo interceptor")
+          console.log(error)
+          return throwError(error);
+        })) as any;
+  }
+}
+
+
+export const ErrorInterceptorProvider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: ErrorInterceptor,
+  multi: true,
+};
+
