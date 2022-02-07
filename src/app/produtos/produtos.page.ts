b153d499bce09d6546ee2faadabaee6deac006f0
RafaelMQs <rafaeelmq08@gmail.com>
2022-02-07 17:49:42 -0300

    Carregando imagens dos produtos

--- src/app/produtos/produtos.page.ts
+++ src/app/produtos/produtos.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavController, NavParams } from '@ionic/angular';
+import { error } from 'protractor';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 
@@ -27,8 +30,17 @@ export class ProdutosPage implements OnInit {
     this.produtoService.findByCategoria(this.currency)
       .subscribe(response => {
         this.items = response['content'];
+        console.log(this.items)
+        this.loadImageUrls();
       },
         error => { });
+  }
+
+  loadImageUrls() {
+    for (var i = 0; i < this.items.length; i++) {
+      let item = this.items[i];
+      item.imageUrl = `assets/imgs/produtos/prod${item.id}.png`;
+    }
   }
 
 }
