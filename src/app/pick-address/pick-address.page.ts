1be152828737d9859e866439ba4c9a37ab3c792b
RafaelMQs <rafaeelmq08@gmail.com>
2022-02-08 17:42:37 -0300

    Buscando endereços do banco de dados

--- src/app/pick-address/pick-address.page.ts
+++ src/app/pick-address/pick-address.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
+import { StorageService } from '../../services/storage.service';
 
 @Component({
   selector: 'app-pick-address',
@@ -10,46 +13,27 @@ export class PickAddressPage implements OnInit {
 
   items: EnderecoDTO[];
 
-  constructor() { }
+  constructor(public storage: StorageService, public clienteService: ClienteService, public navCtrl: NavController) { }
 
   ngOnInit() {
   }
 
   ionViewDidEnter() {
-    this.items = [
-      {
-        id: "1",
-        logradouro: "Rua Quinze de Novembro",
-        numero: "300",
-        complemento: "Apto 200",
-        bairro: "Santa Mônica",
-        cep: "48293822",
-        cidade: {
-          id: "1",
-          nome: "Uberlândia",
-          estado: {
-            id: "1",
-            nome: "Minas Gerais"
+    let localUser = this.storage.getLocalUser();
+    if (localUser && localUser.email) {
+      this.clienteService.findByEmail(localUser.email)
+        .subscribe(response => {
+          this.items = response['enderecos'];
+        },
+          error => {
+            if (error.status == 403) {
+              this.navCtrl.navigateRoot('');
+            }
           }
-        }
-      },
-      {
-        id: "2",
-        logradouro: "Rua Alexandre Toledo da Silva",
-        numero: "405",
-        complemento: null,
-        bairro: "Centro",
-        cep: "88933822",
-        cidade: {
-          id: "3",
-          nome: "São Paulo",
-          estado: {
-            id: "2",
-            nome: "São Paulo"
-          }
-        }
-      }
-    ];
+        )
+    } else {
+      this.navCtrl.navigateRoot('');
+    }
   }
 
 }
